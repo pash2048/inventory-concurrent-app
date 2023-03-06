c8f02fe921525795ef1e832401f6aea791ab3792
Javad <javad@Javads-MacBook-Air.local>
2023-03-06 16:38:29 +0330

    test: Add unit test for canceling inventory addition on interrupt signal
    This commit adds a new unit test, TestCancelOnCtrlC, to test whether an interrupt signal (e.g. ctrl+c) can stop a goroutine that adds inventories and prints out the total inventory. The test creates a context, listens for interrupts using a channel, and creates two goroutines - one to wait for the interrupt signal and cancel the context, and another to add inventories until the context is cancelled. The test verifies that the expected behavior occurs by checking that the total inventory is 0 after the goroutines are done.

--- main_test.go
+++ main_test.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"context"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestConcurrentAddToInventory(t *testing.T) {
@@ -28,4 +34,37 @@ func TestConcurrentAddToInventory(t *testing.T) {
 	if totalInventory != expectedTotal {
 		t.Errorf("Expected total inventory %d, but got %d", expectedTotal, totalInventory)
 	}
+}
+
+func TestCancelOnCtrlC(t *testing.T) {
+	totalInventory = 0
+	// This test verifies whether an interrupt signal (e.g. ctrl+c) can stop a goroutine that adds inventories and prints out the total inventory.
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+
+	// Create a channel to listen for interrupts (e.g. ctrl+c)
+	c := make(chan os.Signal, 1)
+
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	// Create a goroutine that waits for an interrupt and cancels the context
+	go func() {
+		<-c // Wait for a signal to be received
+		cancelFunc()
+	}()
+
+	// Create another goroutine that adds inventories while the context is not cancelled
+	go func() {
+		defer wg.Done()     // Mark the goroutine as done when it exits
+		addInventories(ctx) // Call a function to add inventories until the context is cancelled
+	}()
+
+	// Wait for a second before sending an interrupt to the channel
+	time.Sleep(1 * time.Second)
+	c <- syscall.SIGTERM // Send an interrupt to the channel
+
+	wg.Wait()
+	assert.Equal(t, 0, totalInventory)
 }
