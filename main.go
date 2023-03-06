c9a571b4d482168f8355b9ee8990b16b4ac144f9
Javad <javad@Javads-MacBook-Air.local>
2023-03-06 15:44:03 +0330

    feat: Add concurrency to AddToInventory function
    This commit modifies the AddToInventory function to run concurrently using goroutines. The function now adds inventory items concurrently, improving the performance of the program. The function is thread-safe using a mutex to ensure safe access to the totalInventory variable. This change allows the program to process inventory items more quickly and efficiently.

--- main.go
+++ main.go
@@ -4,26 +4,36 @@ import (
 	"context"
 	"errors"
 	"log"
+	"sync"
 	"time"
 )
 
 var totalInventory int
+var mu sync.Mutex
+var wg sync.WaitGroup
 
 func AddToInventory(_ context.Context, inventory int) {
-	if err := process(inventory); err != nil {
-		log.Print("Inventory is not valid")
-	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := process(inventory); err != nil {
+			log.Print("Inventory is not valid")
+		}
+	}()
 }
 
 func process(inventory int) error {
 	if inventory < 0 {
 		return errors.New("inventory could not be negative")
-	}
 
+	}
 	// It takes two seconds to process the request.
 	time.Sleep(time.Second * 2)
 
-	log.Print("inventory proceed successfully")
+	mu.Lock()
+	defer mu.Unlock()
+
+	log.Print("inventory proceed successfully, ", inventory)
 	totalInventory += inventory
 	return nil
 }
@@ -31,6 +41,7 @@ func process(inventory int) error {
 func main() {
 	ctx := context.Background()
 	addInventories(ctx)
+	wg.Wait()
 }
 
 func addInventories(ctx context.Context) {
